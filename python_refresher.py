d0b39e7cec31ba84198f26733884d61906f092a7
zhumirov <zhumirov@mail.ru>
2021-04-07 15:27:59 +0600

    last commit

---
+++ python_refresher.py
@@ -0,0 +1,15 @@
+age = 311
+
+name = "Bek"
+
+todayIsCold = False
+
+class Car:
+    def _init_(self, year, make, model):
+        self.year = year
+        self.make = make
+        self.model = model
+
+
+    def age(self):
+        return 2020 - self.year
